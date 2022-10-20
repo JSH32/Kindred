58c99d3a526ce6ab9020b69f6b7bd80e3721a19c
J.SH <51795153+JSH32@users.noreply.github.com>
2022-10-20 12:46:55 -0500

    Initial commit

---
+++ Example Mod/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("fabric-loom")
+    kotlin("jvm").version(System.getProperty("kotlin_version"))
+}
+base { archivesName.set(project.extra["archives_base_name"] as String) }
+version = project.extra["mod_version"] as String
+group = project.extra["maven_group"] as String
+repositories {}
+dependencies {
+    minecraft("com.mojang", "minecraft", project.extra["minecraft_version"] as String)
+    mappings("net.fabricmc", "yarn", project.extra["yarn_mappings"] as String, null, "v2")
+    modImplementation("net.fabricmc", "fabric-loader", project.extra["loader_version"] as String)
+    modImplementation("net.fabricmc.fabric-api", "fabric-api", project.extra["fabric_version"] as String)
+    modImplementation("net.fabricmc", "fabric-language-kotlin", project.extra["fabric_language_kotlin_version"] as String)
+}
+tasks {
+    val javaVersion = JavaVersion.VERSION_17
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        sourceCompatibility = javaVersion.toString()
+        targetCompatibility = javaVersion.toString()
+        options.release.set(javaVersion.toString().toInt())
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> { kotlinOptions { jvmTarget = javaVersion.toString() } }
+    jar { from("LICENSE") { rename { "${it}_${base.archivesName}" } } }
+    processResources {
+        inputs.property("version", project.version)
+        filesMatching("fabric.mod.json") { expand(mutableMapOf("version" to project.version)) }
+    }
+    java {
+        toolchain { languageVersion.set(JavaLanguageVersion.of(javaVersion.toString())) }
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+        withSourcesJar()
+    }
+}
